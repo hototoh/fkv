3e8d477bbba5631eb47cb9a836c1602426391c81
tkk.vm.vale <tkk@hongo.wide.ad.jp>
2015-03-21 17:28:10 +0900

    implement put and get method. However, have not done tests yet.

--- circular_log.h
+++ circular_log.h
@@ -6,24 +6,48 @@
 
 #include "mem_alloc.h"
 
+#define CIRCULAR_LOG_FILE "/mnt/hugetlbfs/huge/circular_log"
+#define CIRCULAR_LOG_SIZE 1ULL << 30
+
 typedef struct index_entry {
+  /* value 0 means empty */ 
   uint64_t tag:16;
   uint64_t offset:48;
 } index_entry;
 
-#define ENTRY_SIZE 15
+#define BUCKET_ENTRY_SIZE 15
 typedef struct bucket {
   uint64_t version;
-  index_entry entries[ENTRY_SIZE];
+  index_entry entries[BUCKET_ENTRY_SIZE];
 } bucket;
 
-/* circular_log_entry should be multiples of 8 byte
+#define OPTIMISTIC_LOCK(v, vv, x)                                       \
+  do {                                                                  \
+    v = x->version & (~0x1ULL);                                         \
+    vv = version | 0x1ULL;                                              \
+  } while(__sync_bool_compare_and_swap(&x->version, v, vv));
+
+#define OPTIMISTIC_UNLOCK(x)                    \
+  do {                                          \
+    __sync_add_and_fetch(&x->version, 1);       \
+  } while(0)
+
+/* circular_log_entry should be multiples of 8 bytea
  * for memory alingment !?
  */
 typedef struct circular_log_entry {
   uint64_t initial_size;
+
+  /** key-hash used for multiple purpose
+   * # For server queue selection : 16bits
+        partition(exclusive) | core(not exclusive) index as UDP port
+   * # For bucket selection : (16, 32)? bits 
+   * # For filtering lookup keys : (16, 32)? bits 
+        match index_entry->tag.
+   */
   uint64_t keyhash;
-  uint64_t kv_length;
+  uint64_t key_length:32;
+  uint64_t val_length:32;
   uint64_t expire;
 
   /* [key][value] this length should be 8 multiple */  
@@ -33,6 +57,7 @@ typedef struct circular_log_entry {
 typedef struct circular_log {
   mem_allocator* allocator;
   // circular log size
+  uint64_t addr;
   uint64_t len; 
   uint64_t head;
   uint64_t tail;  
@@ -45,13 +70,22 @@ void
 destroy_circular_log(circular_log* log_table);
 
 bool
-put_circular_log(circular_log* log_table, circular_log_entry* entry);
-
-circular_log_entry*
-get_circular_log(circular_log* log_table, circular_log_entry* entry);
+put_circular_log_entry(circular_log* log_table, bucket* bucket,
+                       circular_log_entry* entry);
 
 bool
-delete_circular_log(circular_log* log_table, circular_log_entry* entry);
+get_circular_log_entry(circular_log* log_table, bucket* bucket,
+                       circular_log_entry* entry);
+
+bool
+remove_circular_log_entry(circular_log* log_table, bucket* bucket,
+                          circular_log_entry* entry);
+
+typedef struct kv_table {
+  circular_log* log;
+  uint64_t bucket_size;
+  bucket buckets[0];
+} kv_table;
 
 
 #endif
