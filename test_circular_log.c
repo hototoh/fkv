99c894302bbe5af49fe090fd0e310172c2ece87a
tkk.vm.vale <tkk@hongo.wide.ad.jp>
2015-03-21 17:40:04 +0900

    add new test.

--- test_circular_log.c
+++ test_circular_log.c
@@ -3,21 +3,37 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <assert.h>
 
+#include "common.h"
 #include "circular_log.h"
 
-int
-main(int argc, char** argv)
-{
+#define LOG_MEM_SIZE (1UL << 30) // 1GB
+#define LOG_FILE_NAME ("/mnt/hugetlbfs/huge/test_circular_log")
+
+void test_circular_log() {
   circular_log* log_table;
-  log_table = create_circular_log("/mnt/hugetlbfs/huge/test_circular_log");
+  log_table = create_circular_log(LOG_FILE_NAME,
+                                  LOG_MEM_SIZE);
   if(log_table == NULL) {
     printf("Failed to create circular_log.");
     return -1;
   }
     
-  D("addr: %lx", log_table->allocator->addr);
-  
-  destroy_mem_allocator(log_table);a
+  D("addr: %lx", (uint64_t) log_table->allocator->addr);
+}
+
+void test_kv_table() {
+
+}
+
+int
+main(int argc, char** argv)
+{
+  D("TEST circular_log");
+  test_circular_log();
+  D("TEST key-value table");
+  test_kv_table();  
+
   return 0;
 }
