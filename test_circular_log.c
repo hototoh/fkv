ebda0758d6bda2da0fa519e9c0865c98b15dbf0f
tkk.vm.vale <tkk@hongo.wide.ad.jp>
2015-03-21 20:21:13 +0900

    add create & destroy for kv_table.

--- test_circular_log.c
+++ test_circular_log.c
@@ -8,8 +8,9 @@
 #include "common.h"
 #include "circular_log.h"
 
-#define LOG_MEM_SIZE (1UL << 30) // 1GB
-#define LOG_FILE_NAME ("/mnt/hugetlbfs/huge/test_circular_log")
+#define LOG_MEM_SIZE (1UL << 29) // 1GB
+#define LOG_FILE_NAME ("/mnt/hugetlbfs/test_circular_log")
+#define BUCKET_SIZE 1024
 
 void test_circular_log() {
   circular_log* log_table;
@@ -17,14 +18,28 @@ void test_circular_log() {
                                   LOG_MEM_SIZE);
   if(log_table == NULL) {
     printf("Failed to create circular_log.");
-    return -1;
+    return ;
   }
     
   D("addr: %lx", (uint64_t) log_table->allocator->addr);
+  destroy_circular_log(log_table);
 }
 
-void test_kv_table() {
+void test_kv_table(uint64_t bucket_size) {
+  kv_table *table = create_kv_table(bucket_size, NULL);
+  if (table == NULL) {
+    D("Fail to create kv_table");
+    assert(false);
+  }
+  D("SUCCESS to create kv_table");
 
+  D("member access check");
+  print_addr(table->log, "log");
+  D("bucket_size: %ld", (uint64_t) table->bucket_size);
+  D("bucket: %lx", (uint64_t) table->buckets);  
+
+  destroy_kv_table(table);
+  D("SUCESS to destroy kv_table");
 }
 
 int
@@ -33,7 +48,7 @@ main(int argc, char** argv)
   D("TEST circular_log");
   test_circular_log();
   D("TEST key-value table");
-  test_kv_table();  
+  test_kv_table(BUCKET_SIZE);  
 
   return 0;
 }
