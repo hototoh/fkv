04a2adc6b7a160a6df3104de8720398f1a006d57
tkk@devg <tkk@devg>
2015-04-07 14:44:59 +0900

    add local benchmark program.

---
+++ localbenchmark.c
@@ -0,0 +1,15 @@
+#ifndef __GNU_SOURCE
+#definee __GNU_SOURCE
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <pthread.h>
+#include <sched.h>
+
+
+
+
