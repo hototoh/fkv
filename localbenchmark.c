3b1f84ae0d3993871ea053a0f78b0a4d3ecc2dc1
hmbp <tkk@hongo.wide.ad.jp>
2015-04-08 16:08:45 +0900

    hoge.

--- localbenchmark.c
+++ localbenchmark.c
@@ -1,5 +1,5 @@
-#ifndef __GNU_SOURCE
-#definee __GNU_SOURCE
+#ifndef _GNU_SOURCE
+#definee _GNU_SOURCE
 #endif
 
 #include <stdio.h>
@@ -11,6 +11,7 @@
 #include <sched.h>
 
 #include "common.h"
+#include "city.h"
 #include "zipf.h"
 #include "mem_manager.h"
 #include "mica_shm.h"
@@ -61,6 +62,12 @@ struct proc_arg
   // size_t num_existing_items;
   // size_t *existing_items;
 };
+
+static inline uint16_t
+get_partition_id(uint64_t key_hash, uint16_t num_partitions)
+{
+  return (uint16_t)(keyhash >> 48) & (uint16_t)(num_partitions - 1);
+}
 
 int
 benchmark_proc(void* _args)
@@ -178,12 +185,24 @@ benchmark_proc(void* _args)
   return 0;
 }
 
-#define NUM_ITEM 1 << 20
-#define BUCKET_SIZE 1 << 17
+static inline circular_log_entry*
+circular_log_entry_at(uint8_t* entries, size_t key_length, size_t val_length
+           uint64_t index)
+{
+  circular_log_entry* entry;
+  uint64_t entry_size = key_length + val_length + sizeof(circular_log_entry);
+  entry = (circular_log_entry*) (entries + entry_size * index);
+  return entry;
+}
+
+#define KEY_SIZE 8
+#define VAL_SIZE 8
+#define NUM_ITEM 1 << 15
+
 void benchmark(int core_num, float zipf_theta, float mth_threshold)
 {
   printf("## Benchmark\n");
-  printf("#  zipf_theta = %lf\n", zipf_theta);
+  printf("*  zipf_theta = %lf\n", zipf_theta);
 
   struct timeval tv_start;
   struct timeval tv_end;
@@ -204,78 +223,9 @@ void benchmark(int core_num, float zipf_theta, float mth_threshold)
   const size_t num_operations = core_num * NUM_ITEM;
   const size_t max_num_operatios_per_thread = num_operations;
 
-  const size_t key_length = MEHCACHED_ROUNDUP8(8);
-  const size_t value_length = MEHCACHED_ROUNDUP8(8);
+  const size_t key_length = KEY_SIZE;
+  const size_t val_length = VAL_SIZE;
 
-  /**
-   * allocating memory for # of the running threads.
-   */
-  printf("allocating memory\n");  
-  uint8_t *keys = (uint8_t *) mehcached_shm_malloc_striped
-                  (key_length * num_items * 2);
-  assert(keys);
-
-  uint64_t *key_hashes = (uint64_t *)mehcached_shm_malloc_striped
-                         (sizeof(uint64_t) * num_items * 2);
-  assert(key_hashes);
-
-  uint16_t *key_parts = (uint16_t *)mehcached_shm_malloc_striped
-                        (sizeof(uint16_t) * num_items * 2);
-  assert(key_parts);
-
-  uint8_t *values = (uint8_t *)mehcached_shm_malloc_striped
-                    (value_length * num_items * 2);
-  assert(values);
-
-  uint64_t *op_count = (uint64_t *)malloc(sizeof(uint64_t) *
-                                          max_num_operatios_per_thread);
-  assert(op_count);
-
-  uint8_t **op_types = (uint8_t **)malloc(sizeof(uint8_t *) *
-                                          max_num_operatios_per_thread);
-  assert(op_types);
-
-  uint8_t **op_keys = (uint8_t **)malloc(sizeof(uint8_t *) *
-                                         max_num_operatios_per_thread);
-  assert(op_keys);
-
-  uint64_t **op_key_hashes = (uint64_t **)malloc(sizeof(uint64_t *) *
-                                                 max_num_operatios_per_thread);
-  assert(op_key_hashes);
-
-  uint16_t **op_key_parts = (uint16_t **)malloc(sizeof(uint16_t *) *
-                                                max_num_operatios_per_thread);
-  assert(op_key_parts);
-  
-  uint8_t **op_values = (uint8_t **)malloc(sizeof(uint8_t *) *
-                                           max_num_operatios_per_thread);
-  assert(op_values);
-
-  size_t thread_id;
-  for (thread_id = 0; thread_id < num_threads; thread_id++)
-  {
-    op_types[thread_id] = (uint8_t *)mehcached_shm_malloc_contiguous(
-        num_operations, thread_id);
-    assert(op_types[thread_id]);
-
-    op_keys[thread_id] = (uint8_t *)mehcached_shm_malloc_contiguous(
-        key_length * num_operations, thread_id);
-    assert(op_keys[thread_id]);
-
-    op_key_hashes[thread_id] = (uint64_t *)mehcached_shm_malloc_contiguous(
-        sizeof(uint64_t) * num_operations, thread_id);
-    assert(op_key_hashes[thread_id]);
-
-    op_key_parts[thread_id] = (uint16_t *)mehcached_shm_malloc_contiguous(
-        sizeof(uint16_t) * num_operations, thread_id);
-    assert(op_key_parts[thread_id]);
-
-    op_values[thread_id] = (uint8_t *)mehcached_shm_malloc_contiguous(
-        value_length * num_operations, thread_id);
-    assert(op_values[thread_id]);
-  }
-
-  size_t mem_diff = (size_t)-1;
   double add_ops = -1.;
   double set_ops = -1.;
   double get_hit_ops = -1.;
@@ -286,20 +236,35 @@ void benchmark(int core_num, float zipf_theta, float mth_threshold)
   double set_1_ops = -1.;
   double get_1_ops = -1.;
 
-  printf("generating %zu items (including %zu miss items)\n",
-         num_items, num_items);
-  for (i = 0; i < num_items * 2; i++)
-  {
-    *(size_t *)(keys + i * key_length) = i;
-    *(key_hashes + i) = hash(keys + i * key_length, key_length);
-    *(key_parts + i) = mehcached_get_partition_id(*(key_hashes + i),
-                                                  (uint16_t)num_partitions);
-    *(size_t *)(values + i * value_length) = i;
+  /**
+   * allocating memory for # of the running threads.
+   */
+  printf("# Allocating memory\n");  
+  char filename[PATH_MAX];
+  sprintf(filename, "test_data_entries");
+  uint64_t data_size = (sizeof(circular_log_entry) + KEY_SIZE + VAL_SIZE) * num_items * 2;
+  uint8_t* log_entries = create_mem_allocator(filename, data_size);
+  assert(log_entries);
+
+  uint64_t *op_count = (uint64_t *)malloc(sizeof(uint64_t) * num_threads);
+  assert(op_count);
+
+  uint8_t **op_types = (uint8_t **)malloc(sizeof(uint8_t *) * num_threas);
+  assert(op_types);
+
+  uint8_t** op_log_entries = (uint8_t**) malloc(sizeof(uint8_t*) * num_threads);
+  assert(op_log_entries);
+
+  for (int i = 0; i < num_threads; i++) {
+    char filename[PATH_MAX];
+    sprintf(filename, "ops_data_entries_%d", core_num);
+    op_log_entries[i] = create_mem_allocator(filename, data_size);
+    assert(ops_log_entries[i]);    
+
+    op_types[i] = (uint8_t *)malloc(sizeof(uint8_t *) * num_operations_per_thread);
+    assert(op_types[i]);
   }
-  printf("\n");
 
-
-  struct proc_arg args[core_num];
   //perf_count_t pc = benchmark_perf_count_init();
   kv_table* table = create_kv_table("localbenchmark", num_threads, BUCKET_SIZE,
                                     BUCKET_SIZE);
@@ -308,76 +273,29 @@ void benchmark(int core_num, float zipf_theta, float mth_threshold)
     return EXIT_FAILURE;
   }
 
-  for (thread_id = 0; thread_id < num_threads; thread_id++)
+  printf("generating %zu items\n", num_items);
+  for (size_t i = 0; i < num_items * 2; i++)
   {
-    args[thread_id].num_threads    = num_threads;
-    args[thread_id].thread_id      = thread_id;
-
-    args[thread_id].key_length     = key_length;
-    args[thread_id].value_length   = value_length;
-
-    args[thread_id].op_types       = op_types[thread_id];
-    args[thread_id].op_keys        = op_keys[thread_id];
-    args[thread_id].op_key_hashes  = op_key_hashes[thread_id];
-    args[thread_id].op_key_parts   = op_key_parts[thread_id];
-    args[thread_id].op_values      = op_values[thread_id];
-    args[thread_id].num_partitions = num_partitions;    
-    
-    args[thread_id].table          = table;
-    args[thread_id].log            = table->logs[thread_id];
+    circular_log_entry* entry = circular_log_entry_at(log_entries, key_length, val_length, i);
+    entry->key_length = key_length;
+    entry->val_length = val_length;
+    memcpy((void*)entry->data, i, key_length);
+    memcpy((void*)((char*)entry->data + key_length), i, val_length);
+    entry->keyhash = CityHash64(entry->data, key_length);
+    entry->initial_size = sizeof(circular_log_entry) + key_length + val_length;
   }
+  printf("\n");
 
-
+  //#######################################################################################################
   benchmark_mode_t benchmark_mode;
-  for (benchmark_mode = 0;
-       benchmark_mode < BENCHMARK_MODE_MAX;
-       benchmark_mode++) {
-    switch (benchmark_mode)
-    {
-      case BENCHMARK_MODE_ADD:
-        printf("adding %zu items\n", num_items);
-        break;
-      case BENCHMARK_MODE_SET:
-        printf("setting %zu items\n", num_items);
-        break;
-      case BENCHMARK_MODE_GET_HIT:
-        printf("getting %zu items (hit)\n", num_items);
-        break;
-      case BENCHMARK_MODE_GET_MISS:
-        printf("getting %zu items (miss)\n", num_items);
-        break;
-      case BENCHMARK_MODE_GET_SET_95:
-        printf("getting/setting %zu items (95%% get)\n", num_items);
-        break;
-      case BENCHMARK_MODE_GET_SET_50:
-        printf("getting/setting %zu items (50%% get)\n", num_items);
-        break;
-      case BENCHMARK_MODE_DELETE:
-        printf("deleting %zu items\n", num_items);
-        break;
-      case BENCHMARK_MODE_SET_1:
-        printf("setting 1 item\n");
-        break;
-      case BENCHMARK_MODE_GET_1:
-        printf("getting 1 item\n");
-        break;
-      default:
-        assert(false);
-    }
-
-    printf("generating workload\n");
-    uint64_t thread_rand_state = 1;
-    //uint64_t key_rand_state = 2;
-    uint64_t op_type_rand_state = 3;
-
-    /* setting the threshold*/
+  for (benchmark_mode_t = 0; benchmark_mode < BENCHMARK_MODE_MAX; benchmark_mode++) {
     uint32_t get_threshold = 0;
-    if (benchmark_mode == BENCHMARK_MODE_ADD    ||
-        benchmark_mode == BENCHMARK_MODE_SET    ||
+    if (benchmark_mode == BENCHMARK_MODE_ADD ||
+        benchmark_mode == BENCHMARK_MODE_SET ||
         benchmark_mode == BENCHMARK_MODE_DELETE ||
         benchmark_mode == BENCHMARK_MODE_SET_1)
       get_threshold = (uint32_t)(0.0 * (double)((uint32_t)-1));
-    else if (benchmark_mode == BENCHMARK_MODE_GET_HIT  ||
+    else if (benchmark_mode == BENCHMARK_MODE_GET_HIT ||
              benchmark_mode == BENCHMARK_MODE_GET_MISS ||
              benchmark_mode == BENCHMARK_MODE_GET_1)
       get_threshold = (uint32_t)(1.0 * (double)((uint32_t)-1));
@@ -388,59 +306,101 @@ void benchmark(int core_num, float zipf_theta, float mth_threshold)
     else
       assert(false);
 
+    printf("generating workload\n");
+    uint64_t op_type_rand_state = 3;
     struct zipf_gen_state zipf_state;
-    mehcached_zipf_init(&zipf_state, num_items,
-                        zipf_theta, (uint64_t)benchmark_mode);
+    mehcached_zipf_init(&zipf_state, num_items, zipf_theta,
+                        (uint64_t)benchmark_mode);
 
-    memset(op_count, 0, sizeof(uint64_t) * num_threads);
-    for (size_t j = 0; j < num_operations; j++)
-    {
-      size_t i;
-      if (benchmark_mode == BENCHMARK_MODE_ADD ||
-          benchmark_mode == BENCHMARK_MODE_DELETE)
+    for (benchmark_mode = 0;
+         benchmark_mode < BENCHMARK_MODE_MAX;
+         benchmark_mode++) {
+      switch (benchmark_mode)
       {
-        if (j >= num_items)
+        case BENCHMARK_MODE_ADD:
+          printf("adding %zu items\n", num_items);
           break;
-        i = j;
+        case BENCHMARK_MODE_SET:
+          printf("setting %zu items\n", num_items);
+          break;
+        case BENCHMARK_MODE_GET_HIT:
+          printf("getting %zu items (hit)\n", num_items);
+          break;
+        case BENCHMARK_MODE_GET_MISS:
+          printf("getting %zu items (miss)\n", num_items);
+          break;
+        case BENCHMARK_MODE_GET_SET_95:
+          printf("getting/setting %zu items (95%% get)\n", num_items);
+          break;
+        case BENCHMARK_MODE_GET_SET_50:
+          printf("getting/setting %zu items (50%% get)\n", num_items);
+          break;
+        case BENCHMARK_MODE_DELETE:
+          printf("deleting %zu items\n", num_items);
+          break;
+        case BENCHMARK_MODE_SET_1:
+          printf("setting 1 item\n");
+          break;
+        case BENCHMARK_MODE_GET_1:
+          printf("getting 1 item\n");
+          break;
+        default:
+          assert(false);
       }
-      else if (benchmark_mode == BENCHMARK_MODE_GET_1 ||
-               benchmark_mode == BENCHMARK_MODE_SET_1)
-        i = 0;
-      else
-      {
-        i = mehcached_zipf_next(&zipf_state);
-        if (benchmark_mode == BENCHMARK_MODE_GET_MISS)
-          i += num_items;
-      }
-
-      uint16_t partition_id = key_parts[i];
-
-      uint32_t op_r = mehcached_rand(&op_type_rand_state);
-      bool is_get = op_r <= get_threshold;
-
-      size_t thread_id = partition_id;
-      if (op_count[thread_id] < max_num_operatios_per_thread)
-      {
-        op_types[thread_id][op_count[thread_id]] = is_get ? 0 : 1;
-        op_key_hashes[thread_id][op_count[thread_id]] = key_hashes[i];
-        op_key_parts[thread_id][op_count[thread_id]] = key_parts[i];
-        memcpy(op_keys[thread_id] + key_length * op_count[thread_id],
-               keys + key_length * i, key_length);
-        memcpy(op_values[thread_id] + value_length * op_count[thread_id],
-               values + value_length * i, value_length);
-        op_count[thread_id]++;
-      }
-      else
-        break;
     }
 
-    D("executing workload");
+    memset(op_count, 0, sizeof(uint64_t)*num_threas);
+    for(size_t i = 0; i < num_operations; i++) {
+      size_t j; 
+      if (benchmark_mode == BENCHMARK_MODE_ADD ||
+          benchmark_mode == BENCHMARK_MODE_DELETE) {
+        if (i >= num_items)
+          break;
+        j = i;
+      } else if (benchmark_mode == BENCHMARK_MODE_GET_1 ||
+                 benchmark_mode == BENCHMARK_MODE_SET_1) {
+        j = 0;
+      } else {
+        j = mehcached_zipf_next(&zipf_state);
+        if (benchmark_mode == BENCHMARK_MODE_GET_MISS)
+          j += num_items;
+      }
 
-    /* start measuring */
+      uint16_t partition_id = get_partition_id(entry->keyhash, num_threads);
+      uint32_t op_rand = mehcaced_rand(&op_type_rand_state);
+      bool is_get = op_rand <= get_threshold;
+    
+      size_t thread_id = partition_id;
+      if (op_count[thread_id] < max_num_operatios_per_thread) {
+        op_types[thred_id][op_count[thread_id]] = is_get ? 0 : 1;
+        circular_log_entry* src_entry = circular_log_entry_at(log_entries, key_length, val_length, j);
+        uint8_t* dst_entry_base = op_log_entries[thread_id];
+        circular_log_entry* dst_entry = circular_log_entry_at(dst_entry_base, key_length, val_length, op_count[thread_id]);
+        memcpy(dst_entry, src_entry, src_entry->initial_size);
+        op_count[thread_id]++;
+      } else break;
+    }
+    printf("\n");
+
+
+    struct proc_arg args[core_num];
+    for (size_t thread_id = 0; thread_id < num_threads; thread_id++)
+    {
+      args[thread_id].num_threads    = num_threads;
+      args[thread_id].thread_id      = thread_id;
+
+      args[thread_id].key_length     = key_length;
+      args[thread_id].value_length   = value_length;
+      args[thread_id].entries        = op_log_entries[thread_id];
+
+      args[thread_id].table          = table;
+      args[thread_id].log            = table->logs[thread_id];
+    }
+
+    D("executing workload");     /* Start measuring */
     running = false;
     for(int thread_id = 0; thread_id < core_num; thread_id++) {    
       struct proc_arg* arg = &args[thread_id];
-      arg
       pthread_create(&arg->thread, NULL, (void*) benchmark_proc, (void*) arg);    
     }
 
@@ -509,13 +469,9 @@ void benchmark(int core_num, float zipf_theta, float mth_threshold)
       default:
         assert(false);
     }
-    /* for (partition_id = 0; partition_id < num_partitions; partition_id++) */
-    /* { */
-    /*   mehcached_print_stats(tables[partition_id]); */
-    /*   mehcached_reset_table_stats(tables[partition_id]); */
-    /* } */
     printf("\n");
   }
+
 
   if (args[0].junk == 1)
     printf("junk: %zu (ignore this line)\n", args[0].junk);
@@ -532,6 +488,7 @@ void benchmark(int core_num, float zipf_theta, float mth_threshold)
   printf("set_1:      %10.2lf Mops\n", set_1_ops * 0.000001);
   printf("get_1:      %10.2lf Mops\n", get_1_ops * 0.000001);
 
+  destroy_kv_table(table);
 }
 
 int main(int argc, char* argv[])
